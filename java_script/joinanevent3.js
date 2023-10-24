8772721e26a162dd1067eba352d88e2b0fdb9d97
Alexa <90927047+alexaa007@users.noreply.github.com>
2023-10-24 12:58:33 +0530

    changed the bg colors

--- java_script/joinanevent3.js
+++ java_script/joinanevent3.js
@@ -1,15 +1,2 @@
-document.getElementById('gender').addEventListener('click', toggleGenderModal);
 
 
-
-function toggleGenderModal() {
-  const genderModal = document.getElementById('gender-modal');
-
-  if(genderModal.classList.contains('hidden')) {
-    genderModal.style.display = "flex";
-    genderModal.classList.remove('hidden');
-  } else {
-    genderModal.style.display = "none";
-    genderModal.classList.add('hidden');
-  }
-}
